f8ce0b2a540b35a163d7de5fc97f9cf8c4c3950e
Nicole <nicole.johnson@epicacademy.org>
2017-10-21 18:47:17 +0000

    add if statement

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,9 +3,14 @@ class TodoController < ApplicationController
  end
  
  def show
- @todo_description = "feed the baby" 
- @todo_pomodoro_estimate = 4 
- end
- 
+     todo_id = params[:id]
+     if todo_id == "1"
+        @todo_description = "feed the baby" 
+        @todo_pomodoro_estimate = 4 
+     elsif todo_id =="2" 
+         @todo_description = "do garza h.w." 
+        @todo_pomodoro_estimate = 2
+     end
+ end 
  
 end

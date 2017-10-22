a5140b04a5f083ca21628c21d695c4997804e5ae
Nicole <nicole.johnson@epicacademy.org>
2017-10-22 18:10:30 +0000

    edit update and destroy flows

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,6 +4,23 @@ class TodoController < ApplicationController
  
  def show
      @todo = Todo.find_by_id(params[:id])
- end 
+ end
  
+ def edit 
+    @todo = Todo.find_by_id(params[:id])
+ end
+ 
+ def update
+ t = Todo.find_by_id(params['id'])
+ t.description = params['description']
+ t.pomodoro_estimate = params['pomodoro-estimate']
+ t.save
+ redirect_to "/todo/show/#{t.id}"
+ end
+ 
+ def destroy 
+ t = Todo.find_by_id(params[:id])
+ t.destroy
+ redirect_to "/todo/index"
+ end
 end

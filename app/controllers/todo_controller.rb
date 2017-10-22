7e39a62fdeb22c4969c96e3a7f9a40f066c09a83
Nicole <nicole.johnson@epicacademy.org>
2017-10-22 15:20:04 +0000

    updated todo controller

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,17 +3,7 @@ class TodoController < ApplicationController
  end
  
  def show
-     todo_id = params[:id]
-     if todo_id == "1"
-        @todo_description = "feed the baby" 
-        @todo_pomodoro_estimate = 4 
-     elsif todo_id =="2" 
-         @todo_description = "do garza h.w." 
-        @todo_pomodoro_estimate = 2
-        elsif todo_id =="3" 
-         @todo_description = "order from mayas" 
-        @todo_pomodoro_estimate = 6
-     end
+     @todo = Todo.find_by_id(params[:id])
  end 
  
 end
